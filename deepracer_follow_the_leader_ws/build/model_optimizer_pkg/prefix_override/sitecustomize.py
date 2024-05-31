89dd875642850e6f86dfa56d5b77efd8f8dd7210
summergerhart <summer.gerhart@dialexa.com>
2024-05-30 20:53:33 -0400

    install attempt 1

---
+++ deepracer_follow_the_leader_ws/build/model_optimizer_pkg/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/root/deepracer_ws/aws-deepracer-follow-the-leader-sample-project/deepracer_follow_the_leader_ws/install/model_optimizer_pkg'
