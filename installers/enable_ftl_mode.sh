df1e87b9eab26f47938e4157c7788af61e52fba9
summergerhart <summer.gerhart@dialexa.com>
2024-07-17 20:32:10 -0500

    update run scripts

--- installers/enable_ftl_mode.sh
+++ installers/enable_ftl_mode.sh
@@ -2,8 +2,8 @@
 echo " ============================"
 echo "| Sourcing the setup scripts |"
 echo " ============================"
-source /opt/ros/foxy/setup.bash 
-source /opt/intel/openvino_2021/bin/setupvars.sh
+# source /opt/ros/foxy/setup.bash 
+# source /opt/intel/openvino_2021/bin/setupvars.sh
 source ~/deepracer_ws/aws-deepracer-follow-the-leader-sample-project/deepracer_follow_the_leader_ws/install/setup.bash
 echo ""
 echo " ====================================="
